e489b6ccd3593965646ed66bb4b4069fda2f975a
Susana Duque <su.duquem@gmail.com>
2021-12-17 10:56:46 -0500

    Refactoring names of helper functions

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -35,11 +35,11 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
   };
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut();
   };
 
@@ -48,7 +48,7 @@ class GoogleAuth extends React.Component {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button onClick={this.onSignOut} className='ui red google button'>
+        <button onClick={this.onSignOutClick} className='ui red google button'>
           <i className='google icon' />
           Sign Outh
         </button>
@@ -58,7 +58,7 @@ class GoogleAuth extends React.Component {
         /* No se le ponen paréntesis al método this.onSignIn, pues se
         ejecutaria ahí mismo que se renderice el componente - comportamiento
         no deseado */
-        <button onClick={this.onSignIn} className='ui red google button'>
+        <button onClick={this.onSignInClick} className='ui red google button'>
           <i className='google icon' />
           Sign In with Google
         </button>
